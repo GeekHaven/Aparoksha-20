bc61e0299496493d31b9982f1d430ad210cf1cde
thenullterminator <dazz2803@gmail.com>
2019-02-15 20:17:31 +0530

    events

--- Main/Events/js/demo.js
+++ Main/Events/js/demo.js
@@ -541,4 +541,37 @@
 			fitWidth: true
 		});
 	});
+
+
+
+	var sidebarBox = document.querySelector('#box'),
+sidebarBtn = document.querySelector('#btn'),
+pageWrapper = document.querySelector('#page-wrapper');
+
+sidebarBtn.addEventListener('click', function (event) {
+		sidebarBtn.classList.toggle('active');
+		sidebarBox.classList.toggle('active');
+});
+
+pageWrapper.addEventListener('click', function (event) {
+
+		if (sidebarBox.classList.contains('active')) {
+				sidebarBtn.classList.remove('active');
+				sidebarBox.classList.remove('active');
+		}
+});
+
+window.addEventListener('keydown', function (event) {
+
+		if (sidebarBox.classList.contains('active') && event.keyCode === 27) {
+				sidebarBtn.classList.remove('active');
+				sidebarBox.classList.remove('active');
+		}
+});
+
+
+
+
+
+
 }
